8ea51a56d9aa44a0d708b362d28dadd7dcecdf35
Marcus <marcfriis@protonmail.com>
2023-03-23 19:15:52 +0100

    removed unnecesary lines

--- lib.py
+++ lib.py
@@ -1,6 +1,5 @@
 import sympy as sp
 
-t = sp.Symbol('t')
 
 def get_cubic_polynomial_weights(start: int, end: int, time: int) -> list[float]:
     """
@@ -13,9 +12,19 @@ def get_cubic_polynomial_weights(start: int, end: int, time: int) -> list[float]
     a3: float = -2 * (end - start) / time**3
     return [a0, a1, a2, a3]
 
-def theta(time, weight: list[float]) -> float:
-    return sp.N(weight[0] + weight[1] * time \
-            + weight[2] * time**2 + weight[3] * time**3, 3)
+def theta(time, weight: list[float]) -> None:
+    sp.pprint(sp.N(weight[0] + weight[1] * time \
+        + weight[2] * time**2 + weight[3] * time**3, 3))
 
-# test = get_cubic_polynomial_weights(15, 75, 3)
-# sp.pprint(theta(t, test))
+def derivative_theta(time, weight: list[float]) -> None:
+    sp.pprint(sp.N(sp.diff(weight[0] + weight[1] * time \
+        + weight[2] * time**2 + weight[3] * time**3), 3))
+
+def double_derivative_theta(time, weight: list[float]) -> None:
+    sp.pprint(sp.N(sp.diff(sp.diff(weight[0] + weight[1] * time \
+        + weight[2] * time**2 + weight[3] * time**3), time), 3))
+
+if __name__ == "__main__":
+    theta(sp.Symbol('t'), get_cubic_polynomial_weights(15, 75, 3))
+    derivative_theta(sp.Symbol('t'), get_cubic_polynomial_weights(15, 75, 3))
+    double_derivative_theta(sp.Symbol('t'), get_cubic_polynomial_weights(15, 75, 3))
