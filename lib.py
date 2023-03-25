ac5c05ff7abb9408c2be7114160a18210d71c388
Marcus <marcfriis@protonmail.com>
2023-03-25 12:21:01 +0100

    merged functions together to remove dublicate code

--- lib.py
+++ lib.py
@@ -1,5 +1,13 @@
 from dataclasses import dataclass
+from enum import Enum, auto, unique
 import sympy as sp
+
+
+@unique
+class Theta(Enum):
+    base = auto()
+    derivative = auto()
+    double_derivative = auto()
 
 
 @dataclass
@@ -17,48 +25,40 @@ class Conditions:
     thetaf: float
     theta0_diff: float = 0
     thetaf_diff: float = 0
-    time: float | None = None
 
 
 class Tragectory_planner:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, conditions: Conditions):
+        self.conditions = conditions
+        self.weights = self.get_weights(conditions)
 
-    def get_weights(self, segment: Conditions) -> Weights:
+    def get_weights(self, conditions: Conditions) -> Weights:
         """Returns weights for a cubic polynomial"""
         return Weights(
-            a0=segment.theta0,
-            a1=segment.theta0_diff,
-            a2=(3 * (segment.thetaf - segment.theta0) / segment.t**2) \
-                - (2 * segment.theta0_diff / segment.t) \
-                - segment.thetaf_diff / segment.t,
-            a3=(-2 * (segment.thetaf - segment.theta0) / segment.t**3) \
-                + (segment.thetaf_diff + segment.theta0_diff) / segment.t**2
+            a0=conditions.theta0,
+            a1=conditions.theta0_diff,
+            a2=(3 * (conditions.thetaf
+                - conditions.theta0) / conditions.t**2)
+            - (2 * conditions.theta0_diff / conditions.t)
+            - conditions.thetaf_diff / conditions.t,
+            a3=(-2 * (conditions.thetaf
+                - conditions.theta0) / conditions.t**3)
+            + (conditions.thetaf_diff
+                + conditions.theta0_diff) / conditions.t**2
         )
 
-    def print_theta(self, segment: Conditions) -> None:
+    def print_theta(self, theta: Theta, time: float | None = None) -> None:
         """Prints theta-angle for specific time or symbolic time"""
-        a = self.get_weights(segment)
-        if segment.time == None: time = sp.Symbol('t') 
-        else: time = segment.time
-        sp.pprint(sp.N(a.a0 + a.a1 * time + a.a2 * time**2 + a.a3 * time**3, 3))
+        a = self.weights
+        tf = time if time else sp.Symbol('t')
 
-    def print_derivative_theta(self, segment: Conditions) -> None:
-        """Prints derivative of theta-angle for specific time or symbolic time"""
-        a = self.get_weights(segment)
-        if segment.time == None: time = sp.Symbol('t') 
-        else: time = segment.time
-        sp.pprint(sp.N(a.a1 + 2 * a.a2 * time + 3 * a.a3 * time**2, 3))
+        if theta is Theta.double_derivative:
+            sp.pprint(sp.N(2 * a.a2 + 6 * a.a3 * tf, 3))
+        elif theta is Theta.derivative:
+            sp.pprint(sp.N(a.a1 + 2 * a.a2 * tf + 3 * a.a3 * tf**2, 3))
+        else:
+            sp.pprint(sp.N(a.a0 + a.a1 * tf + a.a2 * tf**2 + a.a3 * tf**3, 3))
 
-    def print_double_derivative_theta(self, segment: Conditions) -> None:
-        """Prints double derivative of theta-angle for specific time or symbolic time"""
-        a = self.get_weights(segment)
-        if segment.time == None: time = sp.Symbol('t') 
-        else: time = segment.time
-        sp.pprint(sp.N(2 * a.a2 + 6 * a.a3 * time, 3))
 
 if __name__ == "__main__":
-    t = Tragectory_planner()
-    tf = sp.Symbol('tf')
-    t.print_theta(Conditions(5, 15, 1, 0, 15.5, 0.876))
-    t.print_theta(Conditions(15.5, 40, 1, 15.5, 0))
+    t = Tragectory_planner(Conditions(5, 15, 1, 0, 15.5))
