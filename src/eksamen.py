3346a6c310b32f98afc196285e490ad74a7ccd48
Marcus <marcfriis@protonmail.com>
2023-05-28 01:06:18 +0200

    v1 of kinematics lib

---
+++ src/eksamen.py
@@ -0,0 +1,5 @@
+from kinematics import *
+
+Trajectory_cubic_polynomials()
+a = sp.Symbol('a')
+sp.pprint(sp.diff(sp.diff(-2*a+10)))
--- src/kinematics.py
+++ src/kinematics.py
@@ -2,6 +2,8 @@ from dataclasses import dataclass
 import sympy as sp
 import numpy as np
 import math
+import matplotlib.pyplot as plt
+from scipy.spatial.transform import Rotation as R
 
 
 @dataclass
@@ -18,8 +20,8 @@ class Conditions:
     t: float                  # endtime or tf
     theta0: float             # start angle for joint
     thetaf: float             # end angle for joint
-    theta0_diff: float = 0    # 
-    thetaf_diff: float = 0    # 
+    theta0_diff: float = 0    # start velocity
+    thetaf_diff: float = 0    # end velocity
     time: float | None = None # used to define for specifik time
 
 
@@ -36,11 +38,8 @@ class Trajectory_cubic_polynomials:
         return Weights(
             a0=segment.theta0,
             a1=segment.theta0_diff,
-            a2=(3 * (segment.thetaf - segment.theta0) / segment.t**2)
-            - (2 * segment.theta0_diff / segment.t)
-            - segment.thetaf_diff / segment.t,
-            a3=(-2 * (segment.thetaf - segment.theta0) / segment.t**3)
-            + (segment.thetaf_diff + segment.theta0_diff) / segment.t**2
+            a2=(3 * (segment.thetaf - segment.theta0) / segment.t**2) - (2 * segment.theta0_diff / segment.t) - segment.thetaf_diff / segment.t,
+            a3=(-2 * (segment.thetaf - segment.theta0) / segment.t**3) + (segment.thetaf_diff + segment.theta0_diff) / segment.t**2
         )
 
     def print_weights(self, segment: Conditions) -> None:
@@ -55,6 +54,27 @@ class Trajectory_cubic_polynomials:
                   + segment.theta0_diff) / segment.t**2, 3)
 
         sp.pprint(f"a0: {a0} a1:{a1} a2: {a2} a3 {a3}")
+
+    def graph_theta(self, segment: Conditions) -> None:
+        a = self.get_weights(segment)
+        x = np.linspace(0, segment.t, 1000, endpoint=True)
+        y = a.a0 + a.a1 * x + a.a2 * x**2 + a.a3 * x**3
+        plt.plot(x, y)
+        plt.show()
+
+    def graph_derivative_theta(self, segment: Conditions) -> None:
+        a = self.get_weights(segment)
+        x = np.linspace(0, segment.t, 1000, endpoint=True)
+        y = a.a1 + 2 * a.a2 * x + 3 * a.a3 * x**2
+        plt.plot(x, y)
+        plt.show()
+
+    def graph_double_derivative_theta(self, segment: Conditions) -> None:
+        a = self.get_weights(segment)
+        x = np.linspace(0, segment.t, 1000, endpoint=True)
+        y = 2 * a.a2 + 6 * a.a3 * x
+        plt.plot(x, y)
+        plt.show()
 
     def print_theta(self, segment: Conditions) -> None:
         """Prints theta-angle for specific time or symbolic time"""
@@ -79,11 +99,33 @@ class Trajectory_cubic_polynomials:
         sp.pprint(sp.N(2 * a.a2 + 6 * a.a3 * time, 3))
 
 class Trajectory_parabolic_blends:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, theta0: float, thetaf: float, tf: float, acceleration: float) -> None:
+        self.a = acceleration
+        self.theta0 = theta0
+        self.thetaf = thetaf
+        self.tf = tf
+        self.tb = (tf/2) - math.sqrt((self.a**2) * (tf**2) - 4 * self.a * (thetaf - theta0)) / (2 * self.a)
 
-    def print_theta(self) -> None:
-        pass
+    def is_valid_acceleration(self) -> bool:
+        return self.a >= 4 * (self.thetaf - self.theta0) / self.tf**2
+    
+    def get_blendtime(self) -> float:
+        return self.tb
+
+    def get_theta(self, t: float) -> float:
+        """Computes a trapezoidal trajectory, which has a linear motion segment with
+        parabolic blends. Then prints it"""
+        if t > self.tf - self.tb:
+            return self.thetaf - 0.5 * self.a * ((self.tf-t)**2)
+        if t > self.tb:
+            return (0.5 * self.a * (self.tb**2) + self.theta0) + self.a * self.tb * (t-self.tb)
+        return self.theta0 + 0.5 * self.a * (t**2)
+    
+    def graph_theta(self) -> None:
+        x = np.linspace(0, self.tf, 1000, endpoint=True)
+        y = [self.get_theta(t) for t in x]
+        plt.plot(x, y)
+        plt.show()
 
 def T_mdh(alpha: float, a: float, d: float, theta: float) -> np.ndarray:
     """Returns transformation matrix for modified denavit-hartenberg"""
@@ -96,21 +138,24 @@ def T_mdh(alpha: float, a: float, d: float, theta: float) -> np.ndarray:
         [0, 0, 0, 1]
     ])
 
-def rotX(angle: float) -> np.ndarray:
+def rotX(angle: float, degrees: bool = True) -> np.ndarray:
+    if degrees: angle = to_radians(angle)
     return np.array([
         [1, 0, 0],
         [0, math.cos(angle), -math.sin(angle)],
         [0, math.sin(angle), math.cos(angle)]
     ])
 
-def rotY(angle: float) -> np.ndarray:
+def rotY(angle: float, degrees: bool = True) -> np.ndarray:
+    if degrees: angle = to_radians(angle)
     return np.array([
         [math.cos(angle), 0, math.sin(angle)],
         [0, 1, 0],
         [-math.sin(angle), 0, math.cos(angle)]
     ])
 
-def rotZ(angle: float) -> np.ndarray:
+def rotZ(angle: float, degrees: bool = True) -> np.ndarray:
+    if degrees: angle = to_radians(angle)
     return np.array([
         [math.cos(angle), -math.sin(angle), 0],
         [math.sin(angle), math.cos(angle), 0],
@@ -123,24 +168,42 @@ def is_rotation_matrix(matrix: np.ndarray) -> bool:
 
 def to_degrees(number: float) -> float:
     """Converts radians to degrees"""
-    return number*180/math.pi
+    return number * 180/math.pi
 
 def to_radians(number: float) -> float:
     """Converts degrees to radians"""
-    return number*math.pi/180
+    return number * math.pi/180
 
-def to_euler_angles(transformation_matrix: np.ndarray) -> np.ndarray:
-    return
+def to_euler_angles(rotation_matrix: np.ndarray, Conventions: str, degrees: bool) -> np.ndarray:
+    """
+    euler angles are intrinsic whereas fixed-angles are extrinsic
+    example of converntions: zyx, xyx, zyz etc (all combinations of x y z = 12)
+    """
+    r = R.from_matrix(rotation_matrix)
+    return r.as_euler(Conventions.upper(), degrees=degrees)
 
-def to_quaternions(transformation_matrix: np.ndarray) -> np.ndarray:
-    return
+def to_fixed_angless(rotation_matrix: np.ndarray, Conventions: str, degrees: bool) -> np.ndarray:
+    """fixed-angles are extrinsic. fixed: xyz = euler zyx"""
+    r = R.from_matrix(rotation_matrix)
+    return r.as_euler(Conventions.lower(), degrees=degrees)
+
+def to_angle_axis(rotation_matrix: np.ndarray) -> np.ndarray:
+    """rotvec is the same as angle-axis
+    to go the other way use
+    """
+    r = R.from_matrix(rotation_matrix)
+    return r.as_rotvec()
+
+def to_quaternions(rotation_matrix: np.ndarray) -> np.ndarray:
+    r = R.from_matrix(rotation_matrix)
+    return r.as_quat()
 
 
 # jacobians
 
 if __name__ == "__main__":
     np.set_printoptions(precision=3, suppress=True)
-    """
+    
     Xs = 289.48
     Ys = 334.78
     Zs = 818.21
@@ -155,18 +218,41 @@ if __name__ == "__main__":
     Pe = -55.20
     Yawe = 139.95
 
-    tf = (((Xe-Xs)**2 + (Ye - Ys)**2 + (Ze - Zs)**2)**0.5)/100
-    t = TragectoryPlanner()
+    #tf = (((Xe-Xs)**2 + (Ye - Ys)**2 + (Ze - Zs)**2)**0.5)/100
+    #weigths = Trajectory_cubic_polynomials().graph_double_derivative_theta(Conditions(1, 15, 75))
     # tf = sp.Symbol('tf')
-    t.print_weights(Conditions(tf, Xs, Xe))
+    """t.print_weights(Conditions(tf, Xs, Xe))
     t.print_weights(Conditions(tf, Ys, Ye))
     t.print_weights(Conditions(tf, Zs, Ze))
     t.print_weights(Conditions(tf, Rs*math.pi/180, Re*math.pi/180))
     t.print_weights(Conditions(tf, Ps*math.pi/180, Pe*math.pi/180))
     t.print_weights(Conditions(tf, Yaws*math.pi/180, Yawe*math.pi/180))"""
 
-    print(is_rotation_matrix(np.array([
+    """print(is_rotation_matrix(np.array([
         [0, 0, 1],
         [1, 0, 0],
         [0, 1, 0]
-    ])))
+    ])))"""
+    para = Trajectory_parabolic_blends(15, 75, 10, 3)
+    print(para.get_blendtime())
+    print(para.get_theta(0))
+    #para.graph_theta()
+
+    # from euler to rotation matrix. upper-case for intrinsic and lower for extrinsic. angles given in the order of the string
+    #                          z    y   x
+    eul = R.from_euler('ZYX', [30, -30, 45], degrees=True)
+    #                                 x    y   z
+    fixed_axis = R.from_euler('xyz', [45, -30, 30], degrees=True)
+    #print(fixed_axis.as_matrix())
+    #print(eul.as_matrix())
+
+    # example of cubic polyunomial with viapoint. total time is 8 seconds (3 and 5) and with velocity of 10 in the viapoint
+    Trajectory_cubic_polynomials().print_theta(Conditions(3, 15, 75, 0, 10))
+    Trajectory_cubic_polynomials().print_theta(Conditions(5, 75, 35, 10, 0))
+
+
+    ############################################################################
+    Trajectory_cubic_polynomials().print_theta(Conditions(1, 25, 15, 0, -30))
+
+    print(Trajectory_parabolic_blends(-50, 140, 8, 20).get_theta(0.5))
+    Trajectory_parabolic_blends(-50, 140, 8, 20).graph_theta()
